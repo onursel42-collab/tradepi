ec4d1263037ca10281b54e3a1f43c593a98d0dd8
onursel42-collab <onursel42@gmail.com>
2025-12-27 18:09:44 +0300

    Delete frontend/src directory

--- frontend/src/api/packages.js
+++
@@ -1,5 +0,0 @@
-// packages.js - Paket listesi çekme fonksiyonu
-export async function getPackages() {
-  // Gerçek backend entegrasyonu için URL değiştirin
-  return fetch("/api/package").then(res => res.json());
-}
--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar() {
-  return (
-    <nav style={{ background: "#eee", padding: "1em" }}>
-      <span>Tradepi App</span>
-    </nav>
-  );
-}
