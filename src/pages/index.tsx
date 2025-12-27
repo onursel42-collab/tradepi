09b5d73d4433d73e3d6c5a9edc2361a690762bb5
onursel42-collab <onursel42@gmail.com>
2025-12-27 21:23:14 +0300

    Add Navigate component for redirection to '/expo'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
 import { history } from 'umi';
+import { Navigate } from 'umi';
 
+  return <Navigate to="/expo" replace />;
+};
 export default function Index() {
   useEffect(() => {
     history.replace('/expo');
   }, []);
   return null;
 }
+return <Navigate to="/expo" replace />;
